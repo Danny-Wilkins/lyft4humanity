402d272139d8a8cadf6d6948ad52cc9a8b0b189e
Iman Nandi <iman_nandi@yahoo.com>
2016-09-11 07:42:19 -0400

    more

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for, request
+import random
 app = Flask(__name__)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('ride_request'))
+    return render_template('login.html', error=error)
+@app.route('/request', methods=['GET', 'POST'])
+def ride_request():
+    bal = 10
+    if request.method == 'POST' and bal>0: #check balance here
+        return redirect(url_for('redeem_ride'))
+
+    return render_template('request.html')
+@app.route('/redeem', methods=['GET', 'POST'])
+def redeem_ride():
+    error = None
+
+    randNum = "%.8d" % random.randrange(1000000,99999999)
+    return render_template('success.html').format(code=randNum)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+app.run(debug=True, host='0.0.0.0', port = 9000)
---
+++ static/js/backend.js
@@ -0,0 +1,41 @@
+/*$(document).ready(function() {
+    $("#submitButton").click(function() {
+        $.ajax({
+            type: "GET",
+            url: "/v1/find",
+            data: {
+            },
+            success: function(data) {
+                
+            }
+        });
+    });
+});
+*/
+
+var call;
+//var call = alert("This is the typeform API."); //checks for click submit
+$(document).ready(function() {
+    //$(".button-wrapper submit").click(function() {
+        httpGetAsync('https://api.typeform.com/v1/form/z5pYTY?key=4b169802839212949e0bd45b7872426972fe2db3', call);
+    //});
+});
+
+$(document).ready(function() {
+    $(".button-wrapper submit").click(function() {
+        httpGetAsync('https://api.typeform.com/v1/form/z5pYTY?key=4b169802839212949e0bd45b7872426972fe2db3', call);
+    });
+});
+
+function httpGetAsync(theUrl, callback)
+{   
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            //callback(xmlHttp.responseText);
+            console.log(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+    //console.log(xmlHttp.responseText);
+}
